b3a279158ecd2ffc35aa754068c35154601eac1c
Nikolay <nikolayshumik@gmail.com>
2023-10-24 00:02:22 +0300

    add translation to bd models

--- appflut/webflut/models.py
+++ appflut/webflut/models.py
@@ -6,7 +6,7 @@ from django.utils.crypto import get_random_string
 class Personal_Inform(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)  # Add this line
     sex = models.TextField()
-    date_of_birth = models.TextField()
+    date_of_birth = models.DateField()
     weight = models.IntegerField()
     height = models.IntegerField()
 
@@ -15,37 +15,59 @@ class Personal_Inform(models.Model):
 
     # YOUR LIFESTYLE
     active = models.TextField()
-
+    class Meta:
+        verbose_name = 'Личная Информация'
+        verbose_name_plural = 'Личная Информация'
+    def __str__(self):
+        return self.user
 
 
 
 class Add_Product(models.Model):
     name = models.TextField()
-    calories_in = models.TextField()
-    proteins = models.TextField()
-    fats = models.TextField()
-    carbohydrates = models.TextField()
-
+    calories_in = models.IntegerField()
+    proteins = models.IntegerField()
+    fats = models.IntegerField()
+    carbohydrates = models.IntegerField()
+    class Meta:
+        verbose_name = 'Продукты Все'
+        verbose_name_plural = 'Продукты Все'
+    def __str__(self):
+        return self.name
 def get_default_user():
     return User.objects.get_or_create(username='default')[0].id
 class Breakfast_Products(models.Model):
     product = models.ForeignKey(Add_Product, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=get_default_user)
     date = models.DateTimeField(default=timezone.now)
-    weight = models.TextField(default=0)
-
+    weight = models.IntegerField(default=0)
+    class Meta:
+        verbose_name = 'Продукты Завтрака'
+        verbose_name_plural = 'Продукты Завтрака'
+    def __str__(self):
+        return self.product.name
 
 class Lunch_Products(models.Model):
     product = models.ForeignKey(Add_Product, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=get_default_user)
     date = models.DateTimeField(default=timezone.now)
-    weight = models.TextField(default=0)
+    weight = models.IntegerField(default=0)
+    class Meta:
+        verbose_name = 'Продукты Обеда'
+        verbose_name_plural = 'Продукты Обеда'
+    def __str__(self):
+        return self.product.name
 
 class Dinner_Products(models.Model):
      product = models.ForeignKey(Add_Product, on_delete=models.CASCADE)
      user = models.ForeignKey(User, on_delete=models.CASCADE, default=get_default_user)
      date = models.DateTimeField(default=timezone.now)
-     weight = models.TextField(default=0)
+     weight = models.IntegerField(default=0)
+     class Meta:
+         verbose_name = 'Продукты Ужина'
+         verbose_name_plural = 'Продукты Ужина'
+     def __str__(self):
+        return self.product.name
 
 
 
@@ -54,14 +76,24 @@ class Snack_Products(models.Model):
     product = models.ForeignKey(Add_Product, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=get_default_user)
     date = models.DateTimeField(default=timezone.now)
-    weight = models.TextField(default=0)
-
+    weight = models.IntegerField(default=0)
+    class Meta:
+        verbose_name = 'Продукты Перекуса'
+        verbose_name_plural = 'Продукты Перекуса'
+    def __str__(self):
+        return self.product.name
 
 
 class Activity(models.Model):
     category = models.CharField(max_length=200)
     activity_type = models.CharField(max_length=200)
     met = models.FloatField()
+
+    class Meta:
+        verbose_name = 'Виды Активностей '
+        verbose_name_plural = 'Виды Активностей'
+    def __str__(self):
+        return self.activity_type
 
 
 # class Activities(models.Model):
